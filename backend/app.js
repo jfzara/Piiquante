1d913464d81408ae7b4fb72908ec92b0c5568b27
jfzara <zarajeanfabrice@gmail.com>
2022-08-31 05:24:23 -0400

    connexion MongoDB réussie

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const app = express();
+const mongoose = require ('mongoose');
+
+//remplacer l'adresse SRV par la vôtre, et la chaîne <PASSWORD> par votre mot de passe utilisateur
+mongoose.connect('mongodb+srv://mongouserP6OC:cCUCa9KIMpRH2Ifl@cluster0.bvjnxhq.mongodb.net/?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
 
 app.use((req, res) => {
     res.json({message:'Tout fonctionne bien'});
 });
+
+
 
 module.exports = app;
